b5ff9a916d6e31243895bd166f743c0b77fe3ca4
IvanovaAdel <adelinaivanova13@mail.ru>
2025-04-17 20:20:25 +0500

    task.py
    1) В цикл добавьте пропуск числа 4 и завершение цикла на числе 9.
    
    2) В цикл добавьте вывод сообщения "Квадрат числа {number} больше 50".
    
    3) Добавьте второй вызов функции, передавая другие значения.

--- task.py
+++ task.py
@@ -4,5 +4,14 @@ def efszdf(number):
     num = 0
     for num in range(number1):
         num + 1
-        print(num*num)
+        num2 =(num*num)
+        print(num2)
+        if num2 > 50:
+            print(f"Квадрат числа {num} больше 50")
+        if num == 4:
+            continue
+        elif num == 9:
+            break
+       
 efszdf(number = 10.0)
+efszdf(number = 18.0)
