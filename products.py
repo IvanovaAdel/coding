7acfa17644147e915f9691f3f4aa809289fe3ce3
IvanovaAdel <adelinaivanova13@mail.ru>
2025-04-22 20:53:16 +0500

    products.py
    Напишите функцию для посчета и вывода общей суммы всех товаров на складе.

--- products.py
+++ products.py
@@ -16,18 +16,24 @@ def total_amount_of_item():
         print(f"Цена товара {product[0]} - всего {full_price}")
 total_amount_of_item()
 
-price = []
+prices = []
 def item_with_the_highest_amount():
-    global price
+    global prices
     for product in products: 
-        full_price = int(product[1]) * int(product[2])
-        price.append(full_price)
-    price.sort()
-    price.reverse()
-    print(price)
+        full_prices = int(product[1]) * int(product[2])
+        prices.append(full_prices)
+    prices.sort()
+    prices.reverse()
+    print(prices)
     for product in products:
-        if (int(product[1]) * int(product[2])) == price[0]:
+        if (int(product[1]) * int(product[2])) == prices[0]:
             name = product[0]
-            if (int(product[1]) * int(product[2])) == price[0]:
-                print(f"Самая большая цена у товара {name} - {price[0]}")
+            if (int(product[1]) * int(product[2])) == prices[0]:
+                print(f"Самая большая цена у товара {name} - {prices[0]}")
 item_with_the_highest_amount()
+
+def total_amount_of_goods():
+    global prices
+    total_prices = sum(prices)
+    print(f"Общая цена товаров - {total_prices}")
+total_amount_of_goods()
