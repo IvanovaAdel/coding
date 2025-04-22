4b2cf8f445e9da09d4fca82035e8d93a15b408e0
IvanovaAdel <adelinaivanova13@mail.ru>
2025-04-22 19:16:43 +0500

    products.py
    Напишите функцию для вычисления и вывода общей стоимости каждого товара на складе (цена × количество).

--- products.py
+++ products.py
@@ -1,4 +1,4 @@
-products = [("Хлеб", 40, 12), ("Молоко", 60, 83), ("Яблоки", 100), 41]
+products = [("Хлеб", 40, 12), ("Молоко", 60, 83), ("Яблоки", 100, 41)]
 for product in products:
     print(f"Товар: {product[0]}, цена: {product[1]}")
 
@@ -9,3 +9,9 @@ def price_less_than_eighty():
         else:
             continue
 print(price_less_than_eighty())
+
+def total_amount_of_item(): 
+    for product in products: 
+        full_price = int(product[1]) * int(product[2])
+        print(f"Цена товара {product[0]} - всего {full_price}")
+print(total_amount_of_item())
