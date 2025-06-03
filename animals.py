4af6245f136c287b7c455d30fed76bdeb8b9e729
IvanovaAdel <adelinaivanova13@mail.ru>
2025-06-03 16:44:14 +0300

    animals.py

---
+++ animals.py
@@ -0,0 +1,9 @@
+class Animal:
+   def __init__(self, name, species, age):
+       self.name = name
+       self.species = species
+       self.age = age
+cat = Animal("grisha", "fsde", 1)
+print(cat.name)
+print(cat.species)
+print(cat.age)
