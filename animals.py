e4938e3c02c1feb5ac9ccf22e244c19017eb0fc8
IvanovaAdel <adelinaivanova13@mail.ru>
2025-06-03 17:44:10 +0300

    добавлены методы get_...
    и изменила код под изменения

--- animals.py
+++ animals.py
@@ -1,11 +1,21 @@
 class Animal:
     def __init__(self, name, species, age):
-        self.name = name
-        self.species = species
-        self.age = age
+        self.__name = name      
+        self.__species = species 
+        self.__age = age       
 
     def display_info(self):
-        print(f'Имя: {self.name}, Вид: {self.species}, Возраст: {self.age}')
+
+        print(f'Имя: {self.__name}, Вид: {self.__species}, Возраст: {self.__age}')
+        
+    def get_name(self): 
+        return self.__name
+    
+    def get_species(self):
+        return self.__species
+        
+    def get_age(self):
+        return self.__age
 
 class Shelter:
     def __init__(self):
@@ -24,21 +34,25 @@ class Shelter:
     def find_by_species(self, species_name):
         found = False
         for animal in self.animals:
-            if animal.species == species_name:
+            if animal.get_species() == species_name:
                 animal.display_info()
                 found = True
         
         if not found:
             print(f"Нет животных вида '{species_name}'")
+        
+            
+
+
 shelter = Shelter()
 
 cat = Animal("grisha", "Кот", 1)
 cat2 = Animal("pasha", "Кот", 1)
 dog = Animal("Yura", "Собака", 4)
 
-print(cat.name)
-print(cat.species)
-print(cat.age)
+print(cat.get_name())
+print(cat.get_species())
+print(cat.get_age())
 cat.display_info()
 
 shelter.add_animals(cat)
