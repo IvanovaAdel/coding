4b8a5e2c1e6d1112a241aedd199748d7adc454cc
IvanovaAdel <adelinaivanova13@mail.ru>
2025-06-03 17:13:36 +0300

    animals.py
    в этом обновлении я добавила метод добавления животного в приют, через метод add_animals

--- animals.py
+++ animals.py
@@ -1,18 +1,27 @@
 class Animal:
     def __init__(self, name, species, age):
-       self.name = name
-       self.species = species
-       self.age = age
+        self.name = name
+        self.species = species
+        self.age = age
 
     def display_info(self):
-        print(f'Имя: {self.name}, Вид:{self.species} , Возраст:{self.age} ')
-        
+        print(f'Имя: {self.name}, Вид: {self.species}, Возраст: {self.age}')
+
 class Shelter:
     def __init__(self):
         self.animals = []
-        
+
+    def add_animals(self, animal):
+        self.animals.append(animal)
+
+shelter = Shelter()
+
 cat = Animal("grisha", "fsde", 1)
+
 print(cat.name)
 print(cat.species)
 print(cat.age)
-print(cat.display_info())
+cat.display_info()
+
+shelter.add_animals(cat)
+print(shelter.animals)
