757f567a5ce7bf14353a70c02a203b0d9dae56f2
IvanovaAdel <adelinaivanova13@mail.ru>
2025-06-03 16:55:00 +0300

    animals.py
    я создала метод display_into, который при обращении к нему выдает полные данные заданного животного.

--- animals.py
+++ animals.py
@@ -1,9 +1,15 @@
 class Animal:
-   def __init__(self, name, species, age):
+    def __init__(self, name, species, age):
        self.name = name
        self.species = species
        self.age = age
+
+    def display_info(self):
+        print(f'Имя: {self.name}, Вид:{self.species} , Возраст:{self.age} ')
+        
+    
 cat = Animal("grisha", "fsde", 1)
 print(cat.name)
 print(cat.species)
 print(cat.age)
+print(cat.display_info())
