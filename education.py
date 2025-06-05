f501e650c2d005b6d4f74fe2ec5d2d1b58e62be2
IvanovaAdel <adelinaivanova13@mail.ru>
2025-06-05 19:20:59 +0500

    создан метод show_student
    в этом методе можно узнать информацию о каждом студенте группы. Если она пуста, то выведется "Группа пуста".

--- education.py
+++ education.py
@@ -13,7 +13,17 @@ class Group:
     def add_student(self, student):
         self.students.append(student)
         print(self.students)
+        
+    def show_students(self):
+        if not self.students:
+            print("Группа пуста.")
+        else:
+            for student in self.students:
+                student.display_info()
 group = Group()
 stud1 = Student("Grisha", 1)
+stud2 = Student("Masha" , 2)
 stud1.display_info()
 group.add_student(stud1)
+group.add_student(stud2)
+group.show_students()
