ef728b886b234ab8cc98c09094e3ce3de40a2a07
IvanovaAdel <adelinaivanova13@mail.ru>
2025-06-05 20:29:16 +0500

    добавлены приватные данные и гетеры к ним
    в этом обновлении добавлен доступ к приватным переменным с помощью get_..

--- education.py
+++ education.py
@@ -1,17 +1,26 @@
 class Student:
     def __init__(self, name, student_id):
-        self.name = name
-        self.student_id = student_id
-        self.grades = []
+        self.__name = name
+        self.__student_id = student_id
+        self.__grades = []
+        
+    def get_name(self):
+        return self.__name
+        
+    def get_student_id(self):
+        return self.__student_id
+        
+    def get_grades(self):
+        return self.__grades
         
     def display_info(self):
-        print(f"Имя: {self.name}, ID: {self.student_id}")
+        print(f"Имя: {self.__name}, ID: {self.__student_id}")
         
     def add_grade(self, grade):
-        self.grades.append(grade)
+        self.__grades.append(grade)
         
     def get_average(self):
-        return sum(self.grades) / len(self.grades)
+        return sum(self.__grades) / len(self.__grades)
         
 class Group:
     def __init__(self):
@@ -19,7 +28,7 @@ class Group:
     
     def add_student(self, student):
         self.students.append(student)
-        print(f"Добавлен студент: {student.name}")
+        print(f"Добавлен студент: {student.get_name()}") 
         
     def show_students(self):
         if not self.students:
@@ -28,17 +37,22 @@ class Group:
             for student in self.students:
                 student.display_info()
 
+
 group = Group()
 stud1 = Student("Grisha", 1)
 stud2 = Student("Masha", 2)
+
 stud1.add_grade(5)
 stud1.add_grade(4)
 stud1.add_grade(3)
 stud2.add_grade(4)
 stud2.add_grade(5)
+
 stud1.display_info()
 group.add_student(stud1)
 group.add_student(stud2)
+
 group.show_students()
+
 print(f"Средний балл Grisha: {stud1.get_average()}")
 print(f"Средний балл Masha: {stud2.get_average()}")
