4221ac8fb81bd13ec2dc5094ac1946abaa3a1d97
IvanovaAdel <adelinaivanova13@mail.ru>
2025-06-05 18:49:50 +0500

    создан метод дисплей инфо
    в нем можно получить информацию о пользователе

--- education.py
+++ education.py
@@ -1,5 +1,10 @@
 class Student:
     def __init__(self,  name, student_id):
-       self.name = name
+        self.name = name
         self.student_id = student_id
+    
+    def display_info(self):
+        print(f"Имя: {self.name}, ID: {self.student_id}")
         
+stud1 = Student("Grisha", 1)
+stud1.display_info()
