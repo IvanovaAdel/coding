2d65eb90bf490f9d18288c5fab285e17b114a37c
IvanovaAdel <adelinaivanova13@mail.ru>
2025-07-10 20:13:24 +0500

    async.py

--- async.py
+++ async.py
@@ -1,7 +1,20 @@
-import time
+import asyncio
+words_with_delays = [
+    ("Привет", 1.0),
+    ("gggg", 0.5),
+    ("bjhjjh", 2.0),
+    ("iygu", 0.3),
+    ("ghyti", 1.5),
+    ("uifr", 0.7),
+    ("ujoui", 1.2),
+    ("gyuь", 0.9),
+    ("hj", 1.8),
+    ("huh", 0.4),
+]
 
-words = ["Привет", "Привет", "Привет", "Привет", "Привет", "Привет", "Привет", "Привет", "Привет", "Привет"]
+async def print_words():
+    for word, delay in words_with_delays:
+        print(word)
+        await asyncio.sleep(delay)
 
-for word in words:
-    print(word)
-    time.sleep(1)
+asyncio.run(print_words())
