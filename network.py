e510fb1b4205b23845bf387434c89abe228b7dd4
IvanovaAdel <adelinaivanova13@mail.ru>
2025-07-15 21:02:23 +0500

    network.py

---
+++ network.py
@@ -0,0 +1,41 @@
+class Packet:
+  def __init__(self, src, dst, data):
+    self.src = src
+    self.dst = dst
+    self.data = data
+  
+class Computer:
+    def __init__(self, name, router):
+        self.name = name 
+        self.router = router
+
+    def send(self, dst, data):
+        packet = Packet(self.name, dst, data)
+        self.router.route(packet)
+
+    def receive(self, packet):
+        print(f"[{self.name}] Получено сообщение от '{packet.src}': {packet.data}")
+
+class Router:
+    def __init__(self):
+        self.devices = {}
+
+    def connect(self, device):
+        self.devices[device.name] = device
+
+    def route(self, packet):
+        if packet.dst in self.devices:
+            self.devices[packet.dst].receive(packet)
+        else:
+            print(f"[ROUTER] Ошибка: получатель '{packet.dst}' не найден!")
+
+router = Router()
+
+pavel = Computer("Pavel", router)
+bogdan = Computer("Bogdan", router)
+router.connect(pavel)
+router.connect(bogdan)
+pavel.send("Pavel", "Привет, Богдан!")
+bogdan.send("Bogdan", "Здравствуйте, Павел! Скоро будет восстание машин.")
+
+pavel.send("Eve", "Ты меня слышишь?")
